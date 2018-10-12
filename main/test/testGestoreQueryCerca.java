5d365ef6b3c26e5ff601e3551bfa24ef693b2e7e
GiaMoscariello <gia.moscariello@gmail.com>
2018-10-12 14:45:22 +0200

    Aggiunti Test Cases per 'eseguiQueryRicercaImpiegatoConnesso'

--- main/test/testGestoreQueryCerca.java
+++ main/test/testGestoreQueryCerca.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import java.sql.SQLException;
 
 import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.assertTrue;
+import static junit.framework.TestCase.fail;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
@@ -14,30 +16,44 @@ public class testGestoreQueryCerca {
 
     private GestoreQueryCerca gestoreQueryCerca;
 
-    @Before
-    public void setUp()  throws Exception {
+    @Before public void setUp()  throws Exception {
         gestoreQueryCerca = new GestoreQueryCerca();
         assertNotNull(gestoreQueryCerca);
     }
 
-    @After
-    public void tearDown() throws  Exception {
+    @After public void tearDown() throws  Exception {
         gestoreQueryCerca = null;
         assertNull(gestoreQueryCerca);
     }
 
-    @Test
-    public void testeseguiQueryRicercaImpiegatoConnesso() {
+    @Test public void testeseguiQueryRicercaImpiegatoConnesso1() {
         Impiegato testAdmin = new Impiegato("admin","admin","V",43);
         Impiegato testResult = null;
         try {
-            testResult = gestoreQueryCerca.eseguiQueryRicercaImpiegatoConnesso(null, "");
+            testResult = gestoreQueryCerca.eseguiQueryRicercaImpiegatoConnesso("admin", "admin");
         } catch ( SQLException e ) {
             e.printStackTrace();
         }
 
         assertEquals(testAdmin,testResult);
+    }
 
+    @Test public void testeseguiQueryImpiegatoConnesso2() {
+        Impiegato testResult = null;
+        try {
+            testResult = gestoreQueryCerca.eseguiQueryRicercaImpiegatoConnesso("","");
+        } catch ( SQLException e ) { e.printStackTrace(); }
+
+        assertNull(testResult);
+    }
+
+    @Test public void testeeseguiQueryImpiegatoConnesso3() {
+
+        Impiegato testResult = null;
+        try {
+            testResult = gestoreQueryCerca.eseguiQueryRicercaImpiegatoConnesso("prova","prova");
+            fail();// Se non lancia un eccezione il test fallisce, ( resultSet.next() deve lanciarla precisamente  )
+        } catch ( SQLException e ) { assertTrue(true); }
 
     }
 }
